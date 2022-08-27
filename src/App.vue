dd30bfcfd3a1b7caeefb263e675a4d3c10104616
PhyoZayYyar <phyozayyyar2021@gmail.com>
2022-08-27 10:59:48 +0630

    Vue Cli3 template refs and what is components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+    <h1 ref="agreet">Hello</h1>
+    <input type="text" ref="name">
+    <button @click="ClickMe">ClickMe</button>
+    <Modal/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+  },
+  methods:{
+    ClickMe(){
+      this.$refs.name.focus()
+      this.$refs.agreet.textContent="Hee Hee"
+    }
+  },
+  components:{
+    Modal
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="drop">
+    <div class="modal">
+        <p>This Modal Content Box</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .drop{
+    position: fixed;
+    top:0;
+    width:100%;
+    height:100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  .modal{
+    width: 400px;
+    margin:100px auto;
+    padding:20px;
+    background-color: aliceblue;
+    border-radius: 10px;
+  }
+</style>
